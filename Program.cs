fb14544da7e655a3f337b109023bcd8ea3ae3fd0
glinh.dang <gialinhdang0201@gmail.com>
2026-09-16 15:26:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿Console.ForegroundColor = ConsoleColor.White; 
+Console.Write("Skriv in en sträng: ");
+string text = Console.ReadLine();
+
+Console.WriteLine();
+
+long total = 0;
+
+for(int x = 0; x < text.Length; x++)
+{
+    if (char.IsDigit(text[x]))
+    {
+        for(int y = x+1; y < text.Length; y++)
+        {
+            if (!char.IsDigit(text[y]))
+            {
+                break;
+            }
+            if (text[x] == text[y])
+            {
+                string nummer = text.Substring(x, y - x + 1);
+
+                Console.Write(text.Substring(0, x));
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+
+                Console.Write(nummer);
+                Console.ForegroundColor = ConsoleColor.Blue;
+
+                Console.WriteLine(text.Substring(y + 1));
+
+                total = total + long.Parse(nummer);
+
+                break;
+            }
+        }
+    }
+}
+Console.WriteLine();
+
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.Write($"Total:  {total}");
+
+Console.WriteLine();
